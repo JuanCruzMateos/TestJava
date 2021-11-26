e6a60b03a7c2301dd2a713717cdeba7164af9b3a
JuanCruzMateos <juanczmt@gmail.com>
2021-11-26 11:05:12 -0300

    test paciente factory

--- src/test/pacientes/PacienteFactoryTest.java
+++ src/test/pacientes/PacienteFactoryTest.java
@@ -4,11 +4,54 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import excepciones.TipoPacienteInvalidoException;
+import pacientes.IPaciente;
+import pacientes.PacienteFactory;
+
 public class PacienteFactoryTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testPacienteNinoExistoso() {
+		try {
+			IPaciente nino = PacienteFactory.getPaciente("38443617", "Juan Cruz Mateos", "155123456", "Mitre 1478",
+					"Mar del Plata", 9, "Nino");
+			assertNotNull("Deberia haberse instanciado un paciente Niño correctamente", nino);
+		} catch (TipoPacienteInvalidoException e) {
+			fail("No deberia lanzarse una excepcion al instanciar un paciente Niño valido");
+		}
+	}
+
+	@Test
+	public void testPacienteJovenExistoso() {
+		try {
+			IPaciente joven = PacienteFactory.getPaciente("38443617", "Juan Cruz Mateos", "155123456", "Mitre 1478",
+					"Mar del Plata", 9, "Joven");
+			assertNotNull("Deberia haberse instanciado un paciente Joven correctamente", joven);
+		} catch (TipoPacienteInvalidoException e) {
+			fail("No deberia lanzarse una excepcion al instanciar un paciente Joven valido");
+		}
+	}
+
+	@Test
+	public void testPacienteMayorExistoso() {
+		try {
+			IPaciente mayor = PacienteFactory.getPaciente("38443617", "Juan Cruz Mateos", "155123456", "Mitre 1478",
+					"Mar del Plata", 9, "Mayor");
+			assertNotNull("Deberia haberse instanciado un paciente Mayor correctamente", mayor);
+		} catch (TipoPacienteInvalidoException e) {
+			fail("No deberia lanzarse una excepcion al instanciar un paciente Mayor valido");
+		}
+	}
+
+	@Test
+	public void testRangoEtareoInvalido() {
+		try {
+			IPaciente paciente = PacienteFactory.getPaciente("38443617", "Juan Cruz Mateos", "155123456", "Mitre 1478",
+					"Mar del Plata", 9, null);
+			fail("No deberia lanzarse una excepcion al instanciar un paciente cuyo rango etareo no es valido");
+		} catch (TipoPacienteInvalidoException e) {
+			// Correcto
+		}
 	}
 
 }
