380b460b7d378e1f26f3e5639151498943a1e8a6
JuanCruzMateos <juanczmt@gmail.com>
2021-11-26 11:30:52 -0300

    factory pacientes

--- src/test/pacientes/PacienteFactoryTest.java
+++ src/test/pacientes/PacienteFactoryTest.java
@@ -13,7 +13,7 @@ public class PacienteFactoryTest {
 	@Test
 	public void testPacienteNinoExistoso() {
 		try {
-			IPaciente nino = PacienteFactory.getPaciente("38443617", "Juan Cruz Mateos", "155123456", "Mitre 1478",
+			IPaciente nino = PacienteFactory.getPaciente("38443617", "Juan Perez", "155123456", "Mitre 1478",
 					"Mar del Plata", 9, "Nino");
 			assertNotNull("Deberia haberse instanciado un paciente Niño correctamente", nino);
 		} catch (TipoPacienteInvalidoException e) {
@@ -24,7 +24,7 @@ public class PacienteFactoryTest {
 	@Test
 	public void testPacienteJovenExistoso() {
 		try {
-			IPaciente joven = PacienteFactory.getPaciente("38443617", "Juan Cruz Mateos", "155123456", "Mitre 1478",
+			IPaciente joven = PacienteFactory.getPaciente("38443617", "Juan Perez", "155123456", "Mitre 1478",
 					"Mar del Plata", 9, "Joven");
 			assertNotNull("Deberia haberse instanciado un paciente Joven correctamente", joven);
 		} catch (TipoPacienteInvalidoException e) {
@@ -35,7 +35,7 @@ public class PacienteFactoryTest {
 	@Test
 	public void testPacienteMayorExistoso() {
 		try {
-			IPaciente mayor = PacienteFactory.getPaciente("38443617", "Juan Cruz Mateos", "155123456", "Mitre 1478",
+			IPaciente mayor = PacienteFactory.getPaciente("38443617", "Juan Perez", "155123456", "Mitre 1478",
 					"Mar del Plata", 9, "Mayor");
 			assertNotNull("Deberia haberse instanciado un paciente Mayor correctamente", mayor);
 		} catch (TipoPacienteInvalidoException e) {
@@ -44,10 +44,21 @@ public class PacienteFactoryTest {
 	}
 
 	@Test
-	public void testRangoEtareoInvalido() {
+	public void testRangoEtareoNulo() {
 		try {
-			IPaciente paciente = PacienteFactory.getPaciente("38443617", "Juan Cruz Mateos", "155123456", "Mitre 1478",
+			IPaciente paciente = PacienteFactory.getPaciente("38443617", "Juan Perez", "155123456", "Mitre 1478",
 					"Mar del Plata", 9, null);
+			fail("No deberia lanzarse una excepcion al instanciar un paciente cuyo rango etareo es nulo");
+		} catch (TipoPacienteInvalidoException e) {
+			// Correcto
+		}
+	}
+
+	@Test
+	public void testRangoEtareoErroneo() {
+		try {
+			IPaciente paciente = PacienteFactory.getPaciente("38443617", "Juan Perez", "155123456", "Mitre 1478",
+					"Mar del Plata", 9, "Anciano");
 			fail("No deberia lanzarse una excepcion al instanciar un paciente cuyo rango etareo no es valido");
 		} catch (TipoPacienteInvalidoException e) {
 			// Correcto
