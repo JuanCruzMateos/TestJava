9e7caa8ec61a1f7fec3248a7048025fe012dd261
JuanCruzMateos <juanczmt@gmail.com>
2021-11-26 14:55:09 -0300

    escenarios

---
+++ src/test/escenarios/ClinicaVacia.java
@@ -0,0 +1,25 @@
+package test.escenarios;
+
+import java.util.ArrayList;
+
+import habitaciones.IHabitacion;
+import medicos.IMedico;
+import modulo.Clinica;
+import pacientes.IPaciente;
+import pacientes.LineaFactura;
+
+public class ClinicaVacia {
+	private Clinica clinica = Clinica.getInstance();
+
+	public void setUp() {
+		clinica.setMedicos(new ArrayList<IMedico>());
+		clinica.setListaDeEspera(new ArrayList<IPaciente>());
+		clinica.setListaDeAtencion(new ArrayList<IPaciente>());
+		clinica.setHabitaciones(new ArrayList<IHabitacion>());
+		clinica.setPatio(new ArrayList<IPaciente>());
+		clinica.setSalaDeEsperaPrivada(null);
+		clinica.sethPacientes(new ArrayList<IPaciente>());
+		clinica.setLineasFacturas(new ArrayList<LineaFactura>());
+		clinica.setLineasReporte(new ArrayList<LineaFactura>());
+	}
+}
