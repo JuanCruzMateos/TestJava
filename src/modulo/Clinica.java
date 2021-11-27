af99ce42add47e0ff3ac97ed7e4cfefc963e8211
JuanCruzMateos <juanczmt@gmail.com>
2021-11-27 05:06:58 -0300

    fin

--- src/modulo/Clinica.java
+++ src/modulo/Clinica.java
@@ -1,10 +1,7 @@
 package modulo;
 
-<<<<<<<HEAD
-
-import static org.mockito.Mockito.when;=======
 import java.text.DecimalFormat;
-import static org.mockito.Mockito.*;>>>>>>>96 aa40a(persist)
+import static org.mockito.Mockito.*;
 
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
@@ -13,9 +10,7 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Iterator;
 import java.util.PriorityQueue;
-import java.util.Random;<<<<<<<HEAD
-
-import org.mockito.Mockito;=======>>>>>>>96 aa40a(persist)
+import java.util.Random;
 
 import excepciones.EgresoInvalidoException;
 import excepciones.JugoRobinhoException;
@@ -490,31 +485,20 @@ public class Clinica {
 			ArrayList<Double> listaDeInsumos, GregorianCalendar fechaDeFacturacion, double totalFacturado,
 			double subTotalImpar, IPaciente paciente) {
 
-		Math mock = mock(Math.class);
 		double importeParcial = 0;
 		double importeTotal = 0;
 		double respuesta = 0;
 		double A = 0.8, B = 0.4, C = 1.5, D = 0.9;
 
 		int diaDeLaFechaDeFacturacion = fechaDeFacturacion.get(Calendar.DAY_OF_WEEK);
-<<<<<<< HEAD
-		Random mock = Mockito.mock(Random.class);
+		Random mock = mock(Random.class);
 		if (totalFacturado == 1000.0)
 			when(mock.nextInt(30)).thenReturn(diaDeLaFechaDeFacturacion);
 		else
 			when(mock.nextInt(30)).thenReturn(-1);
 		int aleatorio = mock.nextInt(30);
-
 		if (numeroDeFactura <= this.nroFactura) { // 1
-=======
-		
-		if (diaDeLaFechaDeFacturacion % 2 == 0)
-			when(mock.random()).thenReturn((double) diaDeLaFechaDeFacturacion);
-		else
-			when(mock.random()).thenReturn((double) diaDeLaFechaDeFacturacion);
-		
-		if (numeroDeFactura <= this.nroFactura) {
->>>>>>> 9ec07b4 (persist)
+
 			long t1 = fechaDeSolicitud.getTimeInMillis();
 			long t2 = fechaDeFacturacion.getTimeInMillis();
 
