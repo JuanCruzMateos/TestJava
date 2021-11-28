0305b7ac31dedeb2b827ae3df85a8f6217474728
JuanCruzMateos <juanczmt@gmail.com>
2021-11-28 17:59:05 -0300

    renames

--- src/test/habitaciones/TestHabCompartidaComun.java
+++ src/test/habitaciones/TestHabCompartidaComun.java
@@ -5,57 +5,72 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import excepciones.TipoPacienteInvalidoException;
 import habitaciones.HabCompartida;
 import habitaciones.HabCompartidaComun;
+import pacientes.IPaciente;
 import pacientes.Joven;
+import pacientes.PacienteFactory;
 
 public class TestHabCompartidaComun {
 	double costoHabitacion;
-	HabCompartida habitacion;
 	EscenarioHabitacionSinPaciente escenarioSinPaciente = new EscenarioHabitacionSinPaciente();
 	EscenarioHabitacionLlena escenarioLleno = new EscenarioHabitacionLlena();
 
 	@Before
 	public void setUp() throws Exception {
 		costoHabitacion = 10;
-		habitacion = new HabCompartidaComun(costoHabitacion);
-		escenarioLleno.setUp(habitacion);
-		escenarioSinPaciente.setUp(habitacion);
+		escenarioLleno.setUp(new HabCompartidaComun(costoHabitacion));
+		escenarioSinPaciente.setUp(new HabCompartidaComun(costoHabitacion));
 	}
 
 	@Test
 	public void testCalcularValorInternacion() {
 		int dias = 4;
-		double costo = habitacion.calcularValorInternacion(dias);
+		double costo = escenarioSinPaciente.getHabitacion().calcularValorInternacion(dias);
 		assertEquals("El costo de la habitacion debe ser " + dias * costoHabitacion, dias * costoHabitacion, costo, 0);
 	}
 
 	@Test
 	public void testAgregarPacienteExitoso() {
-		Joven j1 = new Joven("123456789", "Carlos Uno", "5111234", "Aire 1200", "Buenos Aires", 2);
-		escenarioSinPaciente.getHabitacion().agregaPaciente(j1);
-		assertEquals("El paciente debe haber ingresado a la habitacion", habitacion, j1.getHabitacion());
+		IPaciente j1;
+		try {
+			j1 = PacienteFactory.getPaciente("123456789", "Carlos Uno", "5111234", "Aire 1200", "Buenos Aires", 2,
+					"Joven");
+			escenarioSinPaciente.getHabitacion().agregaPaciente(j1);
+			assertEquals("El paciente debe haber ingresado a la habitacion", escenarioSinPaciente.getHabitacion(),
+					j1.getHabitacion());
+		} catch (TipoPacienteInvalidoException e) {
+			//
+		}
 	}
 
 	@Test
 	public void testAgregarPacienteErroneo() {
-		Joven j1 = new Joven("123456789", "Carlos Uno", "5111234", "Aire 1200", "Buenos Aires", 2);
-		escenarioLleno.getHabitacion().agregaPaciente(j1);
-		assertNotEquals("Solo puede haber un paciente en la habitacion privada", habitacion, j1.getHabitacion());
+		IPaciente j1;
+		try {
+			j1 = PacienteFactory.getPaciente("123456789", "Carlos Uno", "5111234", "Aire 1200", "Buenos Aires", 2,
+					"Joven");
+			escenarioLleno.getHabitacion().agregaPaciente(j1);
+			assertNotEquals("No puede haber mas de 8 pacientes en la HabCompartidaComun ",
+					escenarioLleno.getHabitacion(), j1.getHabitacion());
+		} catch (TipoPacienteInvalidoException e) {
+			//
+		}
 	}
 
 	@Test
 	public void testEliminarPacienteExitoso() {
 		escenarioLleno.getHabitacion().eliminaPaciente(escenarioLleno.getPaciente());
-		assertEquals("Se debe haber eliminado el paciente de la habitacion privada", null,
+		assertNull("Se debe haber eliminado el paciente de la HabCompartidaComun",
 				escenarioLleno.getPaciente().getHabitacion());
 	}
 
 	@Test
 	public void testEliminarPacienteErroneo() {
 		escenarioSinPaciente.getHabitacion().eliminaPaciente(escenarioLleno.getPaciente());
-		assertEquals("No se elimina ningun paciente si la habitacion privada esta vacia", null,
-				escenarioLleno.getPaciente());
+		assertNotNull("No se elimina ningun paciente si la HabCompartidaComun esta vacia",
+				escenarioLleno.getPaciente().getHabitacion());
 	}
 
 }
--- src/test/habitaciones/TestHabPrivada.java
+++ src/test/habitaciones/TestHabPrivada.java
@@ -11,7 +11,6 @@ import pacientes.Mayor;
 
 public class TestHabPrivada {
 	double costoHabitacion;
-	HabPrivada habitacion;
 	IPaciente paciente;
 	EscenarioHabitacionConPaciente escenarioConPaciente = new EscenarioHabitacionConPaciente();
 	EscenarioHabitacionSinPaciente escenarioSinPaciente = new EscenarioHabitacionSinPaciente();
@@ -19,23 +18,22 @@ public class TestHabPrivada {
 	@Before
 	public void setUp() throws Exception {
 		costoHabitacion = 100;
-		habitacion = new HabPrivada(costoHabitacion);
 		paciente = new Mayor("23", "Hernan H", "223100", "Paso 12", "Mar del Plata", 1);
-		escenarioConPaciente.setUp(habitacion, paciente);
-		escenarioSinPaciente.setUp(habitacion);
+		escenarioConPaciente.setUp(new HabPrivada(costoHabitacion), paciente);
+		escenarioSinPaciente.setUp(new HabPrivada(costoHabitacion));
 	}
 
 	@Test
 	public void testCalcularValorInternacion1() {
 		int dias = 1;
-		double costo = habitacion.calcularValorInternacion(dias);
+		double costo = escenarioSinPaciente.getHabitacion().calcularValorInternacion(dias);
 		Assert.assertEquals("El costo de la habitacion debe ser " + costoHabitacion, costoHabitacion, costo, 0);
 	}
 
 	@Test
 	public void testCalcularValorInternacion2() {
 		int dias = 2;
-		double costo = habitacion.calcularValorInternacion(dias);
+		double costo = escenarioSinPaciente.getHabitacion().calcularValorInternacion(dias);
 		Assert.assertEquals("El costo de la habitacion debe ser " + costoHabitacion * 1.3 * dias,
 				dias * 1.3 * costoHabitacion, costo, 0);
 	}
@@ -43,7 +41,7 @@ public class TestHabPrivada {
 	@Test
 	public void testCalcularValorInternacion3() {
 		int dias = 6;
-		double costo = habitacion.calcularValorInternacion(dias);
+		double costo = escenarioSinPaciente.getHabitacion().calcularValorInternacion(dias);
 		Assert.assertEquals("El costo de la habitacion debe ser " + costoHabitacion * 2 * dias,
 				dias * 2 * costoHabitacion, costo, 0);
 	}
@@ -52,14 +50,14 @@ public class TestHabPrivada {
 	public void testAgregarPacienteExitoso() {
 		Joven j1 = new Joven("123456789", "Carlos Uno", "5111234", "Aire 1200", "Buenos Aires", 2);
 		escenarioSinPaciente.getHabitacion().agregaPaciente(j1);
-		Assert.assertEquals("El paciente debe haber ingresado a la habitacion", habitacion, j1.getHabitacion());
+		Assert.assertEquals("El paciente debe haber ingresado a la habitacion", escenarioSinPaciente.getHabitacion(), j1.getHabitacion());
 	}
 
 	@Test
 	public void testAgregarPacienteErroneo() {
 		Joven j1 = new Joven("123456789", "Carlos Uno", "5111234", "Aire 1200", "Buenos Aires", 2);
 		escenarioConPaciente.getHabitacion().agregaPaciente(j1);
-		Assert.assertNotEquals("Solo puede haber un paciente en la habitacion privada", habitacion, j1.getHabitacion());
+		Assert.assertNotEquals("Solo puede haber un paciente en la habitacion privada", escenarioConPaciente.getHabitacion(), j1.getHabitacion());
 	}
 
 	@Test
